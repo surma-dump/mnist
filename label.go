96313d4fa807108ed023cb9420b1d4dd4ff1b736
Alexander Surma <surma@surmair.de>
2013-12-12 20:55:45 +0100

    Implement LabelReader and tests

--- label.go
+++ label.go
@@ -1,9 +1,11 @@
 package mnist
 
 import (
+	"bytes"
 	"encoding/binary"
 	"fmt"
 	"io"
+	"io/ioutil"
 )
 
 const (
@@ -19,16 +21,77 @@ type Label uint8
 type LabelSet []Label
 
 func NewLabelSet(r io.Reader) (LabelSet, error) {
-	if err := validateMagicNumber(r); err != nil {
+	var err error
+	var rs io.ReadSeeker
+	if nrs, ok := r.(io.ReadSeeker); ok {
+		rs = nrs
+	} else {
+		buf, err := ioutil.ReadAll(r)
+		if err != nil {
+			return nil, err
+		}
+		rs = bytes.NewReader(buf)
+	}
+
+	lr := &LabelReader{ReadSeeker: rs}
+	if err := lr.ValidateHeader(); err != nil {
 		return nil, err
 	}
-	return nil, nil
+	ls := make([]Label, lr.Len())
+	for i := range ls {
+		ls[i], err = lr.ReadLabel(i)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return ls, nil
+}
+
+type LabelReader struct {
+	headerValidated bool
+	numLabels       uint32
+	io.ReadSeeker
+}
+
+func (lr *LabelReader) ReadLabel(i int) (Label, error) {
+	if !lr.headerValidated {
+		panic("Need to call ValidateHeader() first")
+	}
+	if _, err := lr.Seek(int64(8+i), 0); err != nil {
+		return Label(0), err
+	}
+	buf := []byte{0}
+	_, err := lr.Read(buf)
+	return Label(uint8(buf[0])), err
+}
+
+func (lr *LabelReader) Len() int {
+	if !lr.headerValidated {
+		panic("Need to call ValidateHeader() first")
+	}
+	return int(lr.numLabels)
+}
+
+func (lr *LabelReader) ValidateHeader() error {
+	if lr.headerValidated {
+		return nil
+	}
+	if _, err := lr.Seek(0, 0); err != nil {
+		return err
+	}
+	if err := validateMagicNumber(lr); err != nil {
+		return err
+	}
+	if err := binary.Read(lr, binary.BigEndian, &lr.numLabels); err != nil {
+		return err
+	}
+	lr.headerValidated = true
+	return nil
 }
 
 func validateMagicNumber(r io.Reader) error {
 	var magic int32
-	err := binary.Read(r, binary.BigEndian, &magic)
-	if err != nil {
+	if err := binary.Read(r, binary.BigEndian, &magic); err != nil {
 		return err
 	}
 	if magic != MAGIC_NUMBER {
--- label_test.go
+++ label_test.go
@@ -6,30 +6,119 @@ import (
 	"testing"
 )
 
-func TestNewLabelSet_WrongMagicNumber(t *testing.T) {
+func TestValidateMagicNumber_WrongMagicNumber(t *testing.T) {
 	data := "NO_THE_MAGIC_NUMBER"
-	_, err := NewLabelSet(bytes.NewReader([]byte(data)))
+	err := validateMagicNumber(bytes.NewReader([]byte(data)))
 	if err != ErrInvalidMagicNumber {
 		t.Fatalf("Unexpected error: %s", err)
 	}
 }
 
-func TestNewLabelSet_InsufficientData(t *testing.T) {
+func TestValidateMagicNumber_InsufficientData(t *testing.T) {
 	data := "X"
-	_, err := NewLabelSet(bytes.NewReader([]byte(data)))
+	err := validateMagicNumber(bytes.NewReader([]byte(data)))
 	if err == nil {
 		t.Fatalf("Unexpected error: %s", err)
 	}
 }
 
-func TestNewLabelSet_CorrectMagicNumber(t *testing.T) {
+func TestValidateMagicNumber_CorrectMagicNumber(t *testing.T) {
 	buf := &bytes.Buffer{}
-	err := binary.Write(buf, binary.BigEndian, MAGIC_NUMBER)
-	if err != nil {
-		t.Fatalf("Could not write to buffer: %s", err)
-	}
-	_, err = NewLabelSet(buf)
+	binary.Write(buf, binary.BigEndian, MAGIC_NUMBER)
+	err := validateMagicNumber(buf)
 	if err != nil {
 		t.Fatalf("Unexpected error: %s", err)
+	}
+}
+
+func TestLabelReader_ReadLabel(t *testing.T) {
+	buf := &bytes.Buffer{}
+	binary.Write(buf, binary.BigEndian, MAGIC_NUMBER)
+	binary.Write(buf, binary.BigEndian, int32(4))
+	binary.Write(buf, binary.BigEndian, []int8{0, 1, 2, 3})
+
+	lr := &LabelReader{
+		ReadSeeker: bytes.NewReader(buf.Bytes()),
+	}
+	if err := lr.ValidateHeader(); err != nil {
+		t.Fatalf("Could not parse data: %s", err)
+	}
+	if n := lr.Len(); n != 4 {
+		t.Fatalf("Unexpected number of labels: %d", n)
+	}
+	for i := 0; i < 4; i++ {
+		if l, err := lr.ReadLabel(i); err != nil {
+			t.Fatalf("Could not read label #%d: %s", i, err)
+		} else if int(l) != i {
+			t.Fatalf("Read unexpected label #%d: %d", i, l)
+		}
+	}
+}
+
+func TestLabelReader_Len_Unvalidated(t *testing.T) {
+	defer func() {
+		if x := recover(); x == nil {
+			t.Fatalf("Unvalidated read did not panic")
+		}
+	}()
+	lr := &LabelReader{}
+	lr.Len()
+}
+
+func TestLabelReader_ReadLabel_Unvalidated(t *testing.T) {
+	defer func() {
+		if x := recover(); x == nil {
+			t.Fatalf("Unvalidated read did not panic")
+		}
+	}()
+	lr := &LabelReader{}
+	lr.ReadLabel(0)
+}
+
+func TestNewLabelReader_Seekable(t *testing.T) {
+	buf := &bytes.Buffer{}
+	binary.Write(buf, binary.BigEndian, MAGIC_NUMBER)
+	binary.Write(buf, binary.BigEndian, int32(4))
+	binary.Write(buf, binary.BigEndian, []int8{0, 1, 2, 3})
+
+	ls, err := NewLabelSet(bytes.NewReader(buf.Bytes()))
+	if err != nil {
+		t.Fatalf("Could not parse data: %s", err)
+	}
+	if n := len(ls); n != 4 {
+		t.Fatalf("Unexpected number of labels: %d", n)
+	}
+	for i := 0; i < 4; i++ {
+		if int(ls[i]) != i {
+			t.Fatalf("Read unexpected label #%d: %d", i, ls[i])
+		}
+	}
+}
+
+type unseekableReader struct {
+	r *bytes.Reader
+}
+
+func (ub *unseekableReader) Read(v []byte) (n int, err error) {
+	return ub.r.Read(v)
+}
+
+func TestNewLabelReader_Unseekable(t *testing.T) {
+	buf := &bytes.Buffer{}
+	binary.Write(buf, binary.BigEndian, MAGIC_NUMBER)
+	binary.Write(buf, binary.BigEndian, int32(4))
+	binary.Write(buf, binary.BigEndian, []int8{0, 1, 2, 3})
+
+	ls, err := NewLabelSet(&unseekableReader{bytes.NewReader(buf.Bytes())})
+	if err != nil {
+		t.Fatalf("Could not parse data: %s", err)
+	}
+	if n := len(ls); n != 4 {
+		t.Fatalf("Unexpected number of labels: %d", n)
+	}
+	for i := 0; i < 4; i++ {
+		if int(ls[i]) != i {
+			t.Fatalf("Read unexpected label #%d: %d", i, ls[i])
+		}
 	}
 }
